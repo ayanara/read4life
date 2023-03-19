020e24152147ce686f03ce84f9e9f0dd8f1c40ea
Ayanara <aya.nathane@gmail.com>
2023-03-18 22:57:42 -0300

    Estiliza home e detalhes do livro

--- frontend/src/pages/BookDetails.jsx
+++ frontend/src/pages/BookDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { getBookApi } from '../API/fetchAPI';
+import '../styles/pages/bookDetails.css'
 
 
 const BookDetails = () => {
@@ -16,7 +17,7 @@ const BookDetails = () => {
   let thumbnail = book?.volumeInfo?.imageLinks && book.volumeInfo.imageLinks.medium;
   if (!book.volumeInfo) return null
   return (
-    <div className="book">
+    <div className="book-details">
       <div className="book-img">
         <img
           src={thumbnail}
@@ -24,9 +25,15 @@ const BookDetails = () => {
           width="500px"
         >
         </img></div>
-      <div className="bottom">
-        <h3 className="title">{book.volumeInfo.title}</h3>
-        <p className="authors">{book.volumeInfo.authors}</p>
+
+      <div className="book-description">
+        <h1 className="title">{book.volumeInfo.title}</h1>
+        <h2 className="authors"><spam>Escrito por: </spam> {book.volumeInfo.authors}</h2>
+        <h2 className="publisher">{book.volumeInfo.publisher}</h2>
+        <h2 className="description">{book.volumeInfo.description}</h2>
+        <h2 className="publishedDate"><spam>Ano da Publicado: </spam> {book.volumeInfo.publishedDate}</h2>
+        <h2 className="pageCount"><spam>Número de páginas: </spam>{book.volumeInfo.pageCount}</h2>
+        
       </div>
     </div>
   )
