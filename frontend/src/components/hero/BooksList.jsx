e549d905e26edd70179ac6a5ea7f5829d7bb6de6
Ayanara <aya.nathane@gmail.com>
2023-10-19 17:12:23 -0300

    estilização e organização dos cards

--- frontend/src/components/hero/BooksList.jsx
+++ frontend/src/components/hero/BooksList.jsx
@@ -4,33 +4,37 @@ import { useNavigate } from 'react-router-dom';
 const BooksList = ({ book }) => {
   const navigate = useNavigate();
   return (
-    <>
-      {
-        book.map((item) => {
-          let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
-          return (
-            <>
-            <div className="cardBook">
-              <img 
-                src={thumbnail} 
-                alt={item.volumeInfo.title} 
-                width="100px"
-                onClick={()=> navigate (`/book/${item.id}`)}
-              >
-              </img>
+    <div className="p-10">
+      <div className="grid grid-cols-4 gap-4">
+        {
+          book.map((item) => {
+            let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+            return (
+              <div className="flex flex-col w-full h-full border border-gray-300 place-items-center">
+                <img
+                  className="p-8"
+                  src={thumbnail}
+                  alt={item.volumeInfo.title}
+                  width="250px"
+                  onClick={() => navigate(`/book/${item.id}`)}
+                >
+                </img>
 
-              <div className="descriptions">
-                <h3 className="title">{item.volumeInfo.title}</h3>
-                <p className="authors">{item.volumeInfo.authors}</p>
-                <p className="price">{item.saleInfo.listPrice?.amount}</p>
+                <div className="flex flex-col justify-between ">
+                  <h2 className="text-lg font-semibold ">Escrito por: <spam className="text-lg font-light">{item.volumeInfo.authors}</spam></h2>
+                  <h2 className="publisher">{item.volumeInfo.publisher}</h2>
+                  <p className="text-sm">{item.volumeInfo.authors}</p>
+                  <h2 className="publishedDate"><spam>Ano da Publicado: </spam> {item.volumeInfo?.publishedDate}</h2>
+                  <h2 className="pageCount"><spam>Número de páginas: </spam>{item.volumeInfo.pageCount}</h2>
+                  <p className="text-lg font-light">R${item.saleInfo.listPrice?.amount}</p>
+                </div>
               </div>
-            </div>
-            </>
-          )
-        })
-      }
+            )
+          })
+        }
 
-    </>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import BooksList from "../../components/hero/BooksList";
-import { searchBookApi } from '../../API/fetchAPI'
 import Header from "../../components/header/Header";
 import { useSnapshot } from "valtio";
 import { store } from '../../store'
@@ -10,11 +9,10 @@ const Home= () => {
   const snapLivros = useSnapshot(store).livros;
 
   return (
-    <>
+    <section>
       <Header/>
-      
       <BooksList book={snapLivros}/>
-    </>
+    </section>
   )
 }
 
